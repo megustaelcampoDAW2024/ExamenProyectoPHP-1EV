eee25b86481abf89eb034549b0b7b468dc559eef
PcClase <pablobejarano2005@gmail.com>
2024-12-13 08:37:43 +0100

    EJ3

--- resources/views/Layout/plantilla.blade.php
+++ resources/views/Layout/plantilla.blade.php
@@ -8,24 +8,24 @@
     </head>
     <body>
         <div class="d-flex">
-            <header class="bg-light p-3">
-                <p class="text-right">Usuario: {{$_SESSION['usuario']}} | @if($_SESSION['status'] == 'A') Administrador @else Operario @endif | <a href="{{miUrl('logOut')}}" class="btn btn-danger btn-sm">Log Out</a></p>
-                <h1 class="text-center">@yield('titulo')</h1>
-            </header>
+            <div class="container-fluid mt-4">
+                @yield('seccion')
+            </div>
             <nav class="navbar navbar-light bg-light flex-column p-3" style="width: 250px;">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link" href="<?=miUrl('listarTareas')?>">Listar Tareas</a></li>
                     @if ($_SESSION['status'] == 'A')
-                        <li class="nav-item"><a class="nav-link" href="<?=miUrl('crearTarea')?>">Crear Tarea</a></li>
-                        <li class="nav-item"><a class="nav-link" href="<?=miUrl('administrarUsuarios')?>">Administrar Usuarios</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?=miUrl('crearTarea')?>">Crear Tarea</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?=miUrl('administrarUsuarios')?>">Administrar Usuarios</a></li>
                     @endif
                     <li class="nav-item"><a class="nav-link" href="<?=miUrl('ayuda')?>">Ayuda</a></li>
                 </ul>
             </nav>
-            <div class="container-fluid mt-4">
-                @yield('seccion')
-            </div>
         </div>
+        <header class="bg-light p-3">
+            <p class="text-right">Usuario: {{$_SESSION['usuario']}} | @if($_SESSION['status'] == 'A') Administrador @else Operario @endif | <a href="{{miUrl('logOut')}}" class="btn btn-danger btn-sm">Log Out</a></p>
+            <h1 class="text-center">@yield('titulo')</h1>
+        </header>
         <footer class="bg-light text-center p-3 mt-4">
             <p>Derechos reservados por megustaelcampo. Registrado &copy; {{ date('Y') }}</p>
         </footer>
